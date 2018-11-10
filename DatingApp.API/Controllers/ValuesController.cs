d545bc4d7f362f2acfe73eaca1305801352c30f1
droden <danieljroden@hotmail.com>
2018-11-09 20:52:52 -0500

    added registering and cancel to home controller

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace MVCDatingApp.API.Controllers
 {
-    [Authorize]
+   
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
